a21157637a3f4a7d688e9fc0cf38a272f445041f
Jake Munoz <jmunozu09@gmail.com>
2019-04-28 10:30:53 +1000

    init

---
+++ src/Main.elm
@@ -0,0 +1,61 @@
+port module Main exposing (main)
+
+import Browser
+import Css exposing (..)
+import Html
+import Html.Styled exposing (Html, div, fieldset, h1, label, option, p, select, text, toUnstyled)
+import Html.Styled.Attributes exposing (css, placeholder)
+import Html.Styled.Events exposing (onClick)
+
+
+main =
+    Browser.element
+        { init = init
+        , view = view >> toUnstyled
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+
+-- Model
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Cmd msg )
+init _ =
+    ( {}, Cmd.none )
+
+
+type Msg
+    = NoOp
+
+
+
+-- Update
+
+
+update : Msg -> Model -> ( Model, Cmd msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div
+        [ css
+            [ displayFlex
+            , justifyContent center
+            , alignItems center
+            , height (px 500)
+            ]
+        ]
+        [ div
+            []
+            [ h1 [] [ text "Parcel Elm!" ]
+            , p [] [ text "elm parcel starter" ]
+            ]
+        ]
